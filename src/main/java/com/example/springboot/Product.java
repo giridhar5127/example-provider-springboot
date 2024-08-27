393e46b332e5a46ba5b1788a696faa489427cfd8
giridhar5127 <giridhar.tippabathuni@tesco.com>
2024-08-27 17:57:51 +0530

    Add desc

--- src/main/java/com/example/springboot/Product.java
+++ src/main/java/com/example/springboot/Product.java
@@ -15,13 +15,15 @@ class Product {
   private String name;
   private String type;
   private String version;
+  private String description;
 
   Product() {}
 
-  Product(Long id, String name, String type, String version) {
+  Product(Long id, String name, String type, String version, String description) {
     this.id = id;
     this.name = name;
     this.type = type;
     this.version = version;
+    this.description = description;
   }
 }
