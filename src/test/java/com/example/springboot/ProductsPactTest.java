e7e650067ec9a507674f843ac10393ef4054464f
giridhar5127 <giridhar.tippabathuni@tesco.com>
2024-08-27 17:33:45 +0530

    Test changes

--- src/test/java/com/example/springboot/ProductsPactTest.java
+++ src/test/java/com/example/springboot/ProductsPactTest.java
@@ -18,10 +18,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Provider("pactflow-example-provider-springboot")
-@PactBroker(scheme = "https", host = "tesco1.pactflow.io",providerBranch = "main",
-  enablePendingPacts = "true",
-  includeWipPactsSince = "2020-01-01",
-  authentication = @PactBrokerAuth(token = "c4de4c6WGXM94qmRlm01Ww"))
+@PactBroker(url = "https://tesco1.pactflow.io", providerBranch = "main",
+        enablePendingPacts = "true",
+        includeWipPactsSince = "2020-01-01",
+        authentication = @PactBrokerAuth(token = "c4de4c6WGXM94qmRlm01Ww"))
 class ProductsPactTest {
 
   @Autowired
