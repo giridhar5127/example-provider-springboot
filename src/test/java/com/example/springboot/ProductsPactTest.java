c260e3deb0ed54988e4648f926fd0f31590d9503
giridhar5127 <giridhar.tippabathuni@tesco.com>
2024-08-28 12:14:20 +0530

    Remove version selectors

--- src/test/java/com/example/springboot/ProductsPactTest.java
+++ src/test/java/com/example/springboot/ProductsPactTest.java
@@ -59,12 +59,4 @@ class ProductsPactTest {
     repository.save(new Product(10L, "test", "product description", "1.0.0", "desc", "1.0", "f"));
   }
 
-  @PactBrokerConsumerVersionSelectors
-  public static SelectorBuilder consumerVersionSelectors() {
-    // Select Pacts for consumers deployed to production with branch 'FEAT-123' 
-    return new SelectorBuilder()
-            .mainBranch()
-            .deployedOrReleased();
-  }
-
 }
